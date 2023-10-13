1267f14c01879c31b04ffdd9738f7b90935ae381
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-13 13:00:00 -0300

    feat: create folder scanner and process functionalities

---
+++ src/formatter.py
@@ -0,0 +1,9 @@
+def format_size(size_in_bytes):
+    if size_in_bytes < 1024:
+        return f"{size_in_bytes:.2f} bytes"
+    elif size_in_bytes < 1024 ** 2:
+        return f"{size_in_bytes / 1024:.2f} KB"
+    elif size_in_bytes < 1024 ** 3:
+        return f"{size_in_bytes / (1024 ** 2):.2f} MB"
+    else:
+        return f"{size_in_bytes / (1024 ** 3):.2f} GB"
